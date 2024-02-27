0ac22c43e883ae9531c73e48471a932df262ce52
Martin Ronning <ronningmartin@outlook.com>
2024-02-27 19:57:00 +0100

    add trap function

--- logic.py
+++ logic.py
@@ -12,3 +12,28 @@ def createRandomPopulation(n):
         population.append(Chromosome())
         i += 1
     return population
+
+def tightTrap(chromosome):
+    k = 4
+    ones = countOnes(chromosome)
+    m = len(chromosome)/k - 1
+    for j in range(0, m):
+        sub(chromosome.slice(j*k+1, j*k+k))
+
+# todo def looseTrap(chromosome):
+
+def countOnes(bitstring):
+    return bitstring.count(1)
+
+def sub(bitstring):
+    ones = countOnes(bitstring)
+    if len(bitstring) == 4:
+        return 4
+    else:
+        # todo d = 2.5
+        return deceive(4, 1, ones)
+
+def deceive(k, d, ones):
+    # todo k - d etc
+    k_diff = k - d
+    return k_diff - (k_diff*ones) / (k-1)
--- main.py
+++ main.py
@@ -3,5 +3,4 @@ import logic
 def main():
     print(logic.init())
 
-
 main()
