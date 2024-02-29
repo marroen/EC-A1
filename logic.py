05a4238d15f8a232d895383c55010d0be19cd292
Martin Ronning <ronningmartin@outlook.com>
2024-02-29 13:33:09 +0100

    add loose trap function

--- logic.py
+++ logic.py
@@ -3,7 +3,17 @@ from chromosome import Chromosome
 
 def init():
     population = create_random_population(10)
-    print("first chromosome fitness: ", tight_trap(population[0]))
+    k = 4
+    # assuming deceptive trap
+    d = 1
+    # assuming non-deceptive trap
+    # d = 2.5
+    
+    tight_fitness = tight_trap(population[0], k, d)
+    loose_fitness = loose_trap(population[0], k, d)
+    print("first chromosome (tight) fitness: ", tight_fitness)
+    print("first chromosome (loose) fitness: ", loose_fitness)
+
 
 def create_random_population(n):
     population = []
@@ -13,13 +23,7 @@ def create_random_population(n):
         i += 1
     return population
 
-def tight_trap(chromosome):
-    k = 4
-    # assuming deceptive trap function
-    d = 1
-    # assuming non-deceptive trap function
-    #d = 2.5
-
+def tight_trap(chromosome, k, d):
     bitstring = chromosome.data
     m = int(len(bitstring)/k - 1)
 
@@ -31,7 +35,21 @@ def tight_trap(chromosome):
 
     return fitness
 
-# todo def loose_trap(chromosome):
+def loose_trap(chromosome, k, d):
+    bitstring = chromosome.data
+    m = int(len(bitstring)/k - 1)
+
+    fitness = 0
+    for j in range(0, m+1):
+        first = bitstring[j]
+        second = bitstring[j+10]
+        third = bitstring[j+20]
+        fourth = bitstring[j+30]
+        substring = [first, second, third, fourth]
+        ones = count_ones(substring)
+        fitness += sub(ones, k, d)
+    return fitness
+
 
 def count_ones(bitstring):
     return bitstring.count(1)
