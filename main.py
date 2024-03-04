c4254ef7942d50d56796e14ccf3b438255b52754
mickelshout <mickelshout@hotmail.com>
2024-03-04 21:54:25 +0100

    March 4th

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import logic
 import util
+import matplotlib.pyplot as plt
+
 
 def main():
     # number of generations
@@ -33,12 +35,12 @@ def main():
     # logic.non_deceptive_tight_trap                160         90
     # logic.deceptive_loose_trap                    1280        1280
     # logic.non_deceptive_loose_trap                200         1280
-    fit_func = logic.non_deceptive_loose_trap
+    fit_func = util.count_ones
 
     # crossover functions format:
     # logic.select_uniform
     # logic.select_two_point
-    cross_func = logic.select_two_point
+    cross_func = logic.select_uniform
 
     while not found and not error:
         logic.init(n, fit_func, cross_func, k, d)
@@ -118,127 +120,19 @@ def main():
     else:
         print(f"failure with population size {n}")                          #soms population size bigger than 1280
 
+    '''fitness_average = [20.22, 22.0, 22.83, 24.5, 25.69, 26.14, 27.87, 28.94, 30.0, 31.0, 32.25, 32.88, 34.0, 34.94, 35.88, 37.19, 37.88, 38.22]
+    plt.plot(range(1, len(fitness_average) + 1), fitness_average, marker='o', linestyle='-')
+
+    # Set the labels for x and y axes
+    plt.xlabel('Index')
+    plt.ylabel('Fitness Average')
+
+    # Set the title of the plot
+    plt.title('Fitness Average')
+
+    # Display the grid
+    plt.grid(True)
+
+    # Show the plot
+    plt.show()'''
 main()
-
-"""
-while s < 20:
-    logic.init(n, fit_func, cross_func, k, d)
-        if logic.stop_succes:                                               # what if the first of the 20 is false but the rest is true
-            t = 0 
-            for i in range (0,19)
-                logic.init(n, fit_func, cross_func, k, d)
-                if t = 1 and logic.stop_failure:
-                    m = n
-                    n = n * 2
-                    if n >= 1280:
-                        s = 21
-                    break
-                elif logic.stop_failure
-                    t += 1
-                if i == 18:
-                    n = (n + m) / 2
-                    if n % 10 != 0:
-                        n = n * 2
-                        s = 20
-        else:
-            if logic.stop_failure:
-                m = n
-                n = n * 2
-                if n >= 1280:
-                    s = 21
-    else:
-        
-                    
-                    
-            n = (n + m) / 2
-            if n % 10 != 0:
-                n = n * 2
-                s += 1
-                found = True
-        if logic.stop_failure:
-            m = n
-            n = n * 2
-        if n >= 1280:
-            break
-    else:
-        if logic.stop_succes:
-            s += 1
-        else:
-            if one_mistake:
-                found = False
-                s = 0
-                one_mistake = False
-                n = n * 2                                       #fouuuuuuttt
-                if n >= 1280:
-                    break
-            else:
-                one_mistake = True
-                s += 1
-                
-                
-    while not found and not error:
-        logic.init(n, fit_func, cross_func, k, d)
-        if logic.stop_failure:
-            logic.init(n, fit_func, cross_func, k, d)
-            if logic.stop_failure and not found_first:
-                m = n
-                n = n * 2
-                if n >= 1280:
-                    error = True
-            elif logic.stop_failure and found_first:
-                if l != 0:
-                    if 10 <= l - n:
-                        n = l
-                        found = True
-                    else:
-                        m = n
-                        n = (l + n) / 2
-            else:
-                for i in range(0, 18):
-                    logic.init(n, fit_func, cross_func, k, d)
-                    if logic.stop_failure:
-                        m = n
-                        n = (l + n) / 2                     #wat als 10?
-                        break
-                    if i == 17:                             # klopt 17?
-                        found_first = True
-                        l = n
-                        n = (n + m) / 2
-                        if n % 10 != 0:
-                            n = l
-                            found = True
-        else:
-            t = 0
-            for i in range(0, 19):
-                logic.init(n, fit_func, cross_func, k, d)
-                if t == 1 and logic.stop_failure:
-                    if logic.stop_failure and not found_first:
-                        m = n
-                        n = n * 2
-                        if n >= 1280:
-                            error = True
-                            break
-                    elif logic.stop_failure and found_first:
-                        if l != 0:
-                            if 10 <= l - n:
-                                n = l
-                                found = True
-                            else:
-                                m = n
-                                n = (l + n) / 2
-                elif logic.stop_failure:
-                    t += 1
-                if i == 18:
-                    found_first = True
-                    l = n
-                    n = (n + m) / 2
-                    if n % 10 != 0:
-                        n = l
-                        found = True
-         
-
-    
-
-
- 
-"""
